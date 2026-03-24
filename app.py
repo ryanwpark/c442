1333f48117e1a265c7f0616d67ceb78c66060649
Ian Brown <ifbrown11@gmail.com>
2026-03-24 12:42:22 -0400

    implement Prometheus and Grafana monitoring

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from flask import Flask, redirect, url_for
 from view.dashboard_view import dashboard_blueprint
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app, endpoint='/metrics')
 
+# Static information as metric
+metrics.info('app_info', 'Budget App info', version='1.0.0')
 app.register_blueprint(dashboard_blueprint)
 
 @app.route('/')
@@ -11,4 +15,4 @@ def index():
     return redirect(url_for('dashboard.show_dashboard'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=False)
